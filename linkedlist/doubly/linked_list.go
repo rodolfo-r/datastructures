6192c1c2fdfc57a439e4b806f27bfc6768e246d3
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2018-09-07 19:08:44 -0500

    Implement doubly linked list

---
+++ linkedlist/doubly/linked_list.go
@@ -0,0 +1,137 @@
+package doubly
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// List is a doubly Linked list.
+type List struct {
+	root Node // sentinel list element
+}
+
+// Node is linked List node.
+type Node struct {
+	Val        int
+	prev, next *Node
+	list       *List
+}
+
+// Prev returns the previous list node or nil.
+func (n *Node) Prev() *Node {
+	if prev := n.prev; n.list != nil && prev != &n.list.root {
+		return prev
+	}
+	return nil
+}
+
+// Next returns the previous list node or nil.
+func (n *Node) Next() *Node {
+	if next := n.next; n.list != nil && next != &n.list.root {
+		return next
+	}
+	return nil
+}
+
+// New creates a Linked List
+func New() *List {
+	l := &List{}
+	l.root.prev, l.root.next = &l.root, &l.root
+
+	return l
+}
+
+// Insert inserts n after at, and returns n.
+func (l *List) Insert(n, at *Node) *Node {
+	next := at.next
+	at.next = n
+	n.list, n.prev, n.next = l, at, next
+	next.prev = n
+
+	return n
+}
+
+// Append adds a node to the tail of a linked List.
+// O(1)
+func (l *List) Append(val int) *Node {
+	return l.Insert(&Node{Val: val}, l.root.prev)
+}
+
+// Prepend adds a node to the front of a Linked List.
+// O(1)
+func (l *List) Prepend(val int) *Node {
+	return l.Insert(&Node{Val: val}, &l.root)
+}
+
+// Search looks for a node that has a value.
+// O(n) time
+func (l *List) Search(val int) (*Node, error) {
+	for n := l.root.next; n != &l.root; n = n.next {
+		if n.Val == val {
+			return n, nil
+		}
+	}
+	return &Node{}, fmt.Errorf("Node with value %v not in list", val)
+}
+
+// Delete removes a node from its Linked List.
+// O(1)
+func (l *List) Delete(n *Node) {
+	n.prev.next, n.next.prev = n.next, n.prev
+	n.prev, n.next, n.list = nil, nil, nil
+}
+
+// Reverse reverses all nodes following node head.
+// O(n) time, O(n) space
+func (l *List) Reverse() {
+	for n := l.root.next; n != &l.root; n = n.prev {
+		n.prev, n.next = n.next, n.prev
+	}
+
+	l.root.next, l.root.prev = l.root.prev, l.root.next
+}
+
+// Each applies a function to every node.
+func (l *List) Each(fun func(*Node)) {
+	for n := l.root.next; n != &l.root; n = n.next {
+		fun(n)
+	}
+}
+
+// Map returns a copy of every node value applied to a function.
+func (l *List) Map(fun func(int) int) *List {
+	mapped := New()
+	l.Each(func(n *Node) {
+		mapped.Append(fun(n.Val))
+	})
+	return mapped
+}
+
+// Copy returns a copy of a linked List.
+func (l *List) Copy() *List {
+	return l.Map(func(num int) int {
+		return num
+	})
+}
+
+// Equals compares every value in both linked Lists.
+// O(n)
+func (l *List) Equals(l2 *List) bool {
+	for n, n2 := l.root.next, l2.root.next; n != &l.root || n2 != &l2.root; n, n2 = n.next, n2.next {
+		if n == &l.root || n2 == &l2.root || n.Val != n2.Val {
+			return false
+		}
+	}
+	return true
+}
+
+// String creates a string representing the linked List.
+func (l *List) String() string {
+	var strs []string
+	for n := l.root.next; n != &l.root; n = n.next {
+		strs = append(strs, strconv.Itoa(n.Val))
+	}
+
+	return strings.Join(strs, " <--> ")
+}
---
+++ linkedlist/doubly/linked_list_test.go
@@ -0,0 +1,321 @@
+package doubly_test
+
+import (
+	"testing"
+
+	"github.com/techmexdev/algos/linkedlist/doubly"
+)
+
+var empty, zero, one, two, three,
+	zeroOne, oneTwo, zeroTwo, twoOne, oneZero,
+	zeroOneTwo, twoOneZero *doubly.List
+
+func init() {
+	empty, zero, one, two = doubly.New(), doubly.New(), doubly.New(), doubly.New()
+
+	zero.Append(0)
+	one.Append(1)
+	two.Append(2)
+
+	zeroOne, oneTwo, zeroTwo = zero.Copy(), one.Copy(), zero.Copy()
+	twoOne, oneZero := two.Copy(), one.Copy()
+	zeroOne.Append(1)
+	oneTwo.Append(2)
+	zeroTwo.Append(2)
+	twoOne.Append(1)
+	oneZero.Append(0)
+
+	zeroOneTwo, twoOneZero = zeroOne.Copy(), twoOne.Copy()
+	zeroOneTwo.Append(2)
+	twoOneZero.Append(0)
+}
+
+func TestAppend(t *testing.T) {
+	tt := []struct {
+		name string
+		list *doubly.List
+		vals []int
+		want *doubly.List
+	}{
+		{
+			name: "Append one and two to empty",
+			list: empty.Copy(),
+			vals: []int{1, 2},
+			want: oneTwo,
+		},
+		{
+			name: "Append one and two to zero",
+			list: zero.Copy(),
+			vals: []int{1, 2},
+			want: zeroOneTwo,
+		},
+		{
+			name: "Append two to zero and one",
+			list: zeroOne.Copy(),
+			vals: []int{2},
+			want: zeroOneTwo,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			for i := 0; i < len(tc.vals); i++ {
+				tc.list.Append(tc.vals[i])
+			}
+			if !tc.list.Equals(tc.want) {
+				t.Fatalf("have %s, want %s", tc.list, tc.want)
+			}
+		})
+	}
+}
+
+func TestPrepend(t *testing.T) {
+	tt := []struct {
+		name string
+		list *doubly.List
+		vals []int
+		want *doubly.List
+	}{
+		{
+			name: "Prepend two and one to empty",
+			list: empty.Copy(),
+			vals: []int{2, 1},
+			want: oneTwo,
+		},
+		{
+			name: "Prepend one and zero to two",
+			list: two.Copy(),
+			vals: []int{1, 0},
+			want: zeroOneTwo,
+		},
+		{
+			name: "Prepend zero to one and two",
+			list: oneTwo.Copy(),
+			vals: []int{0},
+			want: zeroOneTwo,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			for i := 0; i < len(tc.vals); i++ {
+				tc.list.Prepend(tc.vals[i])
+			}
+			if !tc.list.Equals(tc.want) {
+				t.Fatalf("have %s, want %s", tc.list, tc.want)
+			}
+		})
+	}
+}
+
+func TestSearch(t *testing.T) {
+	tt := []struct {
+		name string
+		list *doubly.List
+		val  int
+		err  bool
+	}{
+		{
+			name: "Search for one in empty",
+			list: empty.Copy(),
+			val:  1,
+			err:  true,
+		},
+		{
+			name: "Search for one in one",
+			list: one.Copy(),
+			val:  1,
+			err:  false,
+		},
+		{
+			name: "Search for one in zero one two",
+			list: zeroOneTwo.Copy(),
+			val:  1,
+			err:  false,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			n, err := tc.list.Search(tc.val)
+
+			if err != nil {
+				if !tc.err {
+					t.Fatalf("have error %s, want nil", err)
+				}
+			} else if n.Val != tc.val {
+				t.Fatalf("have %v, want %v", n.Val, tc.val)
+			}
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	tt := []struct {
+		name string
+		list *doubly.List
+		val  int
+		want *doubly.List
+	}{
+		{
+			name: "Delete zero from zero",
+			list: zero.Copy(),
+			val:  0,
+			want: empty,
+		},
+		{
+			name: "Delete zero from zero one",
+			list: zeroOne.Copy(),
+			val:  0,
+			want: one,
+		},
+		{
+			name: "Delete one from zero one",
+			list: zeroOne.Copy(),
+			val:  1,
+			want: zero,
+		},
+		{
+			name: "Delete one from zero one two",
+			list: zeroOneTwo.Copy(),
+			val:  1,
+			want: zeroTwo,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			n, err := tc.list.Search(tc.val)
+			if err != nil {
+				t.Fatalf("failed searching for %v: %s", tc.val, err)
+			}
+
+			tc.list.Delete(n)
+			if !tc.list.Equals(tc.want) {
+				t.Fatalf("have %s, want %s", tc.list, tc.want)
+			}
+		})
+	}
+}
+
+func TestReverse(t *testing.T) {
+	tt := []struct {
+		name string
+		list *doubly.List
+		want *doubly.List
+	}{
+		{
+			name: "Empty",
+			list: empty.Copy(),
+			want: empty,
+		},
+		{
+			name: "Zero",
+			list: zero.Copy(),
+			want: zero,
+		},
+		{
+			name: "Zero One Two",
+			list: zeroOneTwo.Copy(),
+			want: twoOneZero,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.list.Reverse()
+			if !tc.list.Equals(tc.want) {
+				t.Fatalf("have %s, want %s", tc.list, tc.want)
+			}
+		})
+	}
+}
+
+func TestEach(t *testing.T) {
+	zeroZeroZero := doubly.New()
+	zeroZeroZero.Append(0)
+	zeroZeroZero.Append(0)
+	zeroZeroZero.Append(0)
+
+	zeroTenTwenty := doubly.New()
+	zeroTenTwenty.Append(0)
+	zeroTenTwenty.Append(10)
+	zeroTenTwenty.Append(20)
+
+	tt := []struct {
+		name string
+		list *doubly.List
+		fun  func(*doubly.Node)
+		want *doubly.List
+	}{
+		{
+			name: "Set each node val to 0",
+			list: zeroOneTwo.Copy(),
+			fun:  func(n *doubly.Node) { n.Val = 0 },
+			want: zeroZeroZero,
+		},
+		{
+			name: "Multiply each node val by 10",
+			list: zeroOneTwo.Copy(),
+			fun:  func(n *doubly.Node) { n.Val *= 10 },
+			want: zeroTenTwenty,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.list.Each(tc.fun)
+			if !tc.list.Equals(tc.want) {
+				t.Fatalf("have %s, want %s", tc.list, tc.want)
+			}
+		})
+	}
+}
+
+func TestMap(t *testing.T) {
+	zeroZeroZero := doubly.New()
+	zeroZeroZero.Append(0)
+	zeroZeroZero.Append(0)
+	zeroZeroZero.Append(0)
+
+	zeroTenTwenty := doubly.New()
+	zeroTenTwenty.Append(0)
+	zeroTenTwenty.Append(10)
+	zeroTenTwenty.Append(20)
+
+	tt := []struct {
+		name         string
+		list         *doubly.List
+		fun          func(int) int
+		want, mapped *doubly.List
+	}{
+		{
+			name:   "Map each node val to 0",
+			list:   zeroOneTwo.Copy(),
+			fun:    func(num int) int { return 0 },
+			want:   zeroOneTwo.Copy(),
+			mapped: zeroZeroZero.Copy(),
+		},
+		{
+			name:   "Map Multiply each node val by 10",
+			list:   zeroOneTwo.Copy(),
+			fun:    func(num int) int { return num * 10 },
+			want:   zeroOneTwo,
+			mapped: zeroTenTwenty.Copy(),
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			mapped := tc.list.Map(tc.fun)
+
+			if !tc.list.Equals(tc.want) {
+				t.Fatalf("have %s, want %s", tc.list, tc.want)
+			}
+
+			if !mapped.Equals(tc.mapped) {
+				t.Fatalf("have mapped %s, want %s", mapped, tc.mapped)
+			}
+		})
+	}
+}
