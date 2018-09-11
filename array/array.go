11b0e8d63dd5ddbcde7818812fafae98b1d2289d
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2018-09-11 15:56:14 -0500

    array: add array list

---
+++ array/array.go
@@ -0,0 +1,90 @@
+package array
+
+import (
+	"fmt"
+)
+
+// Array represents an array list.
+type Array struct {
+	list []int
+}
+
+const growthFactor = 2
+const shrinkFactor = 4
+
+// New creates a dynamic array.
+func New() *Array {
+	return &Array{}
+}
+
+// Insert adds num to the array list.
+func (a *Array) Insert(num int) {
+	if len(a.list)+1 > cap(a.list) {
+		a.growWith(len(a.list)*growthFactor, num)
+	} else {
+		a.list = append(a.list, num)
+	}
+}
+
+// Search looks for num in the list, and returns it's index,
+// or an error if it doesn't exist.
+func (a *Array) Search(num int) (int, error) {
+	for i := range a.list {
+		if a.list[i] == num {
+			return i, nil
+		}
+	}
+	return -1, fmt.Errorf("num %v not in list", num)
+}
+
+// Get retrieves the number at provided index,
+// or an error if the index is out of bounds.
+func (a *Array) Get(index int) (int, error) {
+	if index >= len(a.list) {
+		return 0, fmt.Errorf("index %v out of bounds", index)
+	}
+	return a.list[index], nil
+}
+
+// Delete removes the element at provided index,
+// and returns an error if index is out of bounds.
+func (a *Array) Delete(index int) error {
+	if index >= len(a.list) || index < 0 {
+		return fmt.Errorf("index %v out of bounds", index)
+	}
+	if shrunkCap := cap(a.list) / shrinkFactor; len(a.list)-1 <= shrunkCap {
+		a.shrinkWithout(shrunkCap, index)
+	} else {
+		a.list = append(a.list[:index], a.list[index+1:]...)
+	}
+	return nil
+}
+
+func (a *Array) growWith(newCap, num int) {
+	if len(a.list) == 0 {
+		a.list = []int{num}
+		return
+	}
+	newList := make([]int, len(a.list)+1, newCap)
+	for i := range a.list {
+		newList[i] = a.list[i]
+	}
+	newList[len(a.list)] = num
+
+	a.list = newList
+}
+
+// shrinkWithout reassigns the Array's list to a new list
+// with cap shrunkCap, and without the element at index.
+func (a *Array) shrinkWithout(newCap, index int) {
+	newList := make([]int, len(a.list)-1, newCap)
+	for i := 0; i < index; i++ {
+		newList[i] = a.list[i]
+	}
+
+	for i := index + 1; i < len(a.list); i++ {
+		newList[i-1] = a.list[i]
+	}
+
+	a.list = newList
+}
---
+++ array/array_test.go
@@ -0,0 +1,80 @@
+package array_test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/techmexdev/algos/array"
+)
+
+func TestNew(t *testing.T) {
+	array.New()
+}
+
+func TestInsert(t *testing.T) {
+	arr := array.New()
+	for n := 0; n < 12; n++ {
+		arr.Insert(n)
+	}
+}
+
+func TestSearch(t *testing.T) {
+	arr := array.New()
+	for n := 0; n < 12; n++ {
+		arr.Insert(n)
+	}
+
+	for n := 0; n < 12; n++ {
+		i, err := arr.Search(n)
+		if err != nil {
+			t.Fatalf("have error: %s, want index %v", err, n)
+		} else if i != n {
+			t.Fatalf("have index: %v, want %v", i, n)
+		}
+	}
+}
+
+func TestGet(t *testing.T) {
+	arr := array.New()
+	for n := 0; n < 12; n++ {
+		arr.Insert(n)
+	}
+
+	for n := 0; n < 12; n++ {
+		num, err := arr.Get(n)
+		if err != nil {
+			t.Fatalf("have error: %s, want num %v", err, num)
+		} else if n != num {
+			t.Fatalf("have num: %v, want %v", num, n)
+		}
+	}
+}
+func TestDelete(t *testing.T) {
+	arr := array.New()
+	for n := 0; n < 12; n++ {
+		arr.Insert(n)
+	}
+
+	for a := 0; a < 12; a++ {
+		num, _ := arr.Get(0)
+		err := arr.Delete(0)
+		if err != nil {
+			t.Fatalf("have error: %s, want nil", err)
+		}
+
+		num, err = arr.Search(num)
+		if err == nil {
+			log.Fatalf("want error, have num %v and nil error", num)
+		}
+	}
+
+	err := arr.Delete(100)
+	if err == nil {
+		t.Fatalf("have error nil, want error")
+	}
+
+	err = arr.Delete(-1)
+	if err == nil {
+		t.Fatalf("have error nil, want error")
+	}
+}
