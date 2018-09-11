cc13963dc0d9d6aa85038d26f50261781dd30d1a
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2018-09-10 22:29:28 -0500

    sort: update Merge

--- sort/merge.go
+++ sort/merge.go
@@ -1,19 +1,15 @@
 package sort
 
 import (
-	"log"
-
 	"github.com/techmexdev/algos/queue"
 )
 
 // Merge applies merge sort to nums.
 func Merge(nums []int) {
 	merge(nums, 0, len(nums)-1)
-	log.Printf("sorted: %v", nums)
 }
 
 func merge(nums []int, start, end int) {
-	log.Printf("start: %v, end: %v", start, end)
 	if start >= end {
 		return
 	}
@@ -24,16 +20,12 @@ func merge(nums []int, start, end int) {
 }
 
 func mergeSorted(nums []int, start, mid, end int) {
-	log.Printf("merge sorted: start: %v, end: %v", start, end)
-
 	q, q2 := queue.New(), queue.New()
 	for i := start; i <= mid; i++ {
 		q.Enqueue(nums[i])
-		log.Printf("q.Enqueue(nums[i]): %v", nums[i])
 	}
 
 	for i := mid + 1; i <= end; i++ {
-		log.Printf("q2.Enqueue(nums[i]): %v", nums[i])
 		q2.Enqueue(nums[i])
 	}
 
@@ -74,6 +66,4 @@ func mergeSorted(nums []int, start, mid, end int) {
 
 		nums[i], i = n, i+1
 	}
-
-	log.Printf("merge sorted: %v", nums[start:end])
 }
